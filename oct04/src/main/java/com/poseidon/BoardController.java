c72d254fc8cbb127421986dd211d46cccd253c4c
chxxah <chxxxah@gmail.com>
2023-10-05 23:33:11 +0900

    [2023-10-05]

--- oct04/src/main/java/com/poseidon/BoardController.java
+++ oct04/src/main/java/com/poseidon/BoardController.java
@@ -13,12 +13,14 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //2023-09-15
 @CrossOrigin
 @RestController
+@RequestMapping("/api")
 public class BoardController {
 
 	@Autowired
